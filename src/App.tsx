dff61cd546030c725c6fa03912a16e591806a951
Jillian Howell <40151421+jilherma@users.noreply.github.com>
2020-12-13 23:24:40 -0500

    finished Nyt challenge

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NytApp from './Components/NytApp';
+import './App.css';
+
+const App: React.FunctionComponent = () =>  {
+  return (
+    <div className="App">
+      <NytApp />
+    </div>
+  );
+}
+
+export default App;
